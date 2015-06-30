89deddd05d8a67e8e84e8085e2a47b608b962a2b
depra95 <acc.depra95@gmail.com>
2015-06-30 11:31:58 +0900

    sassにスタイルを追加, mediaqueryを設定

--- webd/index.php
+++ webd/index.php
@@ -1,8 +1,32 @@
-<html>
- <head>
-  <title>PHP Test</title>
- </head>
- <body>
- <?php echo '<p>Hello World</p>'; ?>
- </body>
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Untitled Document</title>
+    <link rel="stylesheet" type="text/css" href="sass/style.css">
+</head>
+
+<body>
+    <div class="wrapper">
+        <nav class="navigation">
+            <div class="inner inner--navigation">
+                <div class="navigation__title">
+                    aaa
+                </div>
+                <div class="navigation__menu">
+                </div>
+            </div>
+        </nav>
+        <div class="contentswrapper">
+            <main class="contents">
+                <div class="inner">
+                    Untitled Document
+                     <?php echo '<p>Hello World</p>'; ?>
+                </div>
+            </main>
+        </div>
+    </div>
+</body>
+
 </html>
