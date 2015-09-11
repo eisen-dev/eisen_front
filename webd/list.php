f0350a58f9412b0c098e2dad90097c6956aec09c
depra95 <acc.depra95@gmail.com>
2015-09-12 00:18:13 +0900

    headタグ, navigationをパーツ化, list.phpを追加, bootstrap3を追加, bootstrap準拠のCSS記法に変更

---
+++ webd/list.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="ja">
+<?php require_once __DIR__ . '/parts/head.php'; ?>
+<body>
+	<div class="wrapper">
+<?php require_once __DIR__ . '/parts/navigation.php'; ?>
+		<div class="contentswrapper">
+			<main class="contents">
+				<div class="section">
+					<div class="inner inner-section">
+						<h1>Hello</h1> Untitled Document!
+                        <div class="panel panel-default">
+                        <table class="table table-bordered">
+                            <thead>
+                                <tr>
+                                    <th>パッケージカテゴリ</th>
+                                    <th>パッケージカテゴリ</th>
+                                    <th>パッケージカテゴリ</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>aaa</td>
+                                    <td>aaa</td>
+                                    <td>aaa</td>
+                                </tr>
+                                <tr>
+                                    <td>aaa</td>
+                                    <td>aaa</td>
+                                    <td>aaa</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                        </div>
+					</div>
+				</div>
+			</main>
+		</div>
+	</div>
+    <script src="includes/bootstrap-3.3.5-dist/js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="js/script.js"></script>
+</body>
+
+</html>
---
+++ webd/parts/navigation.php
@@ -0,0 +1,73 @@
+<nav class="navigation">
+			<div class="inner">
+				<i class="fa fa-bars navigation-toggle"></i>
+				<div class="navigation-title">
+				</div>
+				<div class="menu clearfix">
+					<div class="menu-button machines menu-border-red">
+						<div class="menu-icon">
+							<i class="fa fa-server menu-icon"></i>
+						</div>
+
+					</div>
+
+					<div class="menu-button menu-border-green">
+						<div class="menu-icon toggle-notifications">
+							<i class="fa fa-bell-o"></i>
+						</div>
+						<div class="notifications-list">
+							<div class="menu-list-title"><i class="fa fa-bell-o fa-inline"></i>Notifications</div>
+							<div class="menu-list-items">
+							<ul>
+								<li>
+									<span class="notifications-machine-name">WebServer</span>
+									<span class="notifications-time">2015/07/07 14:44</span>
+									<br>
+									<span class="notifications-message">
+                                    <i class="fa fa-check-circle fa-inline"></i>
+										パッケージのインストールが完了しました
+									</span>
+								</li>
+								<li>
+									<span class="notifications-machine-name">WebServer</span>
+									<span class="notifications-time">2015/07/07 12:31</span>
+									<br>
+									<span class="notifications-message">
+                                    <i class="fa fa-times-circle fa-inline"></i>
+										パッケージのインストールが失敗しました
+									</span>
+								</li>
+								<li>
+									<span class="notifications-machine-name">WebServer</span>
+									<span class="notifications-time">2015/07/03 09:55</span>
+									<br>
+									<span class="notifications-message">
+                                    <i class="fa fa-exclamation-circle fa-inline"></i>
+										新しいパッケージの更新があります
+									</span>
+								</li>
+								<li>
+									<span class="notifications-machine-name">WebServer</span>
+									<span class="notifications-time">2015/07/02 11:25</span>
+									<br>
+									<span class="notifications-message">
+                                    <i class="fa fa-check-circle fa-inline"></i>
+										パッケージの更新が完了しました
+									</span>
+								</li>
+								<li>
+									<span class="notifications-machine-name">WebServer</span>
+									<span class="notifications-time">2015/07/01 21:19</span>
+									<br>
+									<span class="notifications-message">
+                                    <i class="fa fa-check-circle fa-inline"></i>
+										パッケージのインストールが完了しました
+									</span>
+								</li>
+							</ul>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</nav>
