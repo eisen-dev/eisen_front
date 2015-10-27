a744aef3889be3c7cf4b3fba2aabf5d93b39de08
alice ferrazzi <alice.ferrazzi@gmail.com>
2015-10-27 13:05:27 +0900

    リストの更新です。

--- webd/list.php
+++ webd/list.php
@@ -1,5 +1,13 @@
 <!DOCTYPE html>
 <html lang="ja">
+	<meta charset="UTF-8">
+	<title>パッケージリスト</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+	<link rel="stylesheet" type="text/css" href="includes/normalize.css">
+	<link rel="stylesheet" href="includes/font-awesome-4.3.0/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="sass/style.css">
+	<script type="text/javascript" src="includes/jquery/jquery-2.1.4.min.js"></script>
+</head>
 <?php
 $title = "Untitled Document";
 require_once __DIR__ .'/parts/head.php';
@@ -18,6 +26,26 @@ catch (PDOException $e) {
 <body>
 	<div class="wrapper">
 	<?php require_once __DIR__ .'/parts/navigation.php'; ?>
+	<div class="contentswrapper">
+			<main class="contents menu-set">
+				<div class="section">
+					<h2 class="title">パッケージリスト</h2>
+					<form>
+						<div class="list-tools clearfix">
+							<div class="list-action">
+								<select name="list-action" class="input-list">
+									<option value="0">一括操作</option>
+									<option value="0">更新</option>
+								</select>
+								<input type="submit" value="適用" class="button button--form">
+							</div>
+							<div class="search-box">
+								<input type="text" placeholder="全てのパッケージを検索">
+								<div class="search-box__button">
+								<i class="fa fa-search"></i>
+								</div>
+							</div>
+						</div>
 		<div class="contentswrapper">
 			<main class="contents">
 				<div class="section">
