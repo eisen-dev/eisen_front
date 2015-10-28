aa57e3ca3742e86b1bae658a8fa52365ee3f1466
depra95 <acc.depra95@gmail.com>
2015-10-28 23:36:42 +0900

    index.phpのクラス指定ミスを修正

--- webd/index.php
+++ webd/index.php
@@ -10,43 +10,41 @@ $dbc = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
 	<div class="wrapper">
 <?php require_once __DIR__ .'/parts/navigation.php'; ?>
 		<div class="contentswrapper">
-			<main class="contents  menu-visible">
+			<main class="contents  menu-set">
 				<div class="section">
-					<div class="inner inner-section">
-						<h1>Hello</h1> Untitled Document!
-						<p>
-						<a href="/install.php">install database</a>
-						</p>
-						<p>
-						<a href="/dbupdate.php">update database</a>
-						</p>
-						<p>
-						<a href="/list.php">show list</a>
-						</p>
-						<button class="btn btn-primary" data-toggle="modal" data-target="#myModal">
-							Basic Modals
-						</button>
-						<!-- <?php require_once __DIR__ . '/parts/modal.php'; ?> -->
-						<!-- モーダルウィンドウここから -->
-						<div id="myModal" class="modal fade">
-							<div class="modal-dialog">
-								<div class="modal-content">
-									<div class="modal-header">
-										<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-										<h4 class="modal-title">タイトル</h4>
-									</div>
-									<div class="modal-body">
-										モーダルのbody部分
-									</div>
-									<div class="modal-footer">
-										<button type="button" class="btn btn-default" data-dismiss="modal">閉じる</button>
-										<button type="button" class="btn btn-primary">保存</button>
-									</div>
+					<h1>Hello</h1> Untitled Document!
+					<p>
+					<a href="/install.php">install database</a>
+					</p>
+					<p>
+					<a href="/dbupdate.php">update database</a>
+					</p>
+					<p>
+					<a href="/list.php">show list</a>
+					</p>
+					<button class="btn btn-primary" data-toggle="modal" data-target="#myModal">
+						Basic Modals
+					</button>
+					<!-- <?php require_once __DIR__ . '/parts/modal.php'; ?> -->
+					<!-- モーダルウィンドウここから -->
+					<div id="myModal" class="modal fade">
+						<div class="modal-dialog">
+							<div class="modal-content">
+								<div class="modal-header">
+									<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+									<h4 class="modal-title">タイトル</h4>
+								</div>
+								<div class="modal-body">
+									モーダルのbody部分
+								</div>
+								<div class="modal-footer">
+									<button type="button" class="btn btn-default" data-dismiss="modal">閉じる</button>
+									<button type="button" class="btn btn-primary">保存</button>
 								</div>
 							</div>
 						</div>
-						<!-- モーダルウィンドウここまで -->
 					</div>
+					<!-- モーダルウィンドウここまで -->
 				</div>
 			</main>
 		</div>
