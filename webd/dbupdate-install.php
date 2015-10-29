a7da70b0aa4762fb575fe617c3672d173bb8b617
alice ferrazzi <alice.ferrazzi@gmail.com>
2015-10-30 04:01:31 +0900

    データベース再設計

--- webd/dbupdate-install.php
+++ webd/dbupdate-install.php
@@ -21,24 +21,26 @@ array(PDO::ATTR_EMULATE_PREPARES => false,
 } catch (PDOException $e) {
  exit('データベース接続に失敗しました'.$e->getMessage());
 }
-
+$test=1;
 $data=$server->get_installed_packages();
 foreach ($data as $value){
 	try {
-		$sqlcheck = $dbh->prepare("SELECT COUNT(*) FROM installed_package WHERE installed_pack_category = :installed_pack_cat AND installed_pack_name = :installed_pack_name AND installed_pack_version = :installed_pack_version;");
+		$sqlcheck = $dbh->prepare("SELECT COUNT(*) FROM installed_package WHERE installed_pack_category = :installed_pack_cat AND installed_pack_name = :installed_pack_name AND installed_pack_version = :installed_pack_version AND pack_sys_id = :pack_sys_id ;");
 		//connect as appropriate as above
 		$sqlcheck-> bindParam(':installed_pack_cat', $value[0], PDO::PARAM_STR);
 		$sqlcheck-> bindParam(':installed_pack_name', $value[1], PDO::PARAM_STR);
 		$sqlcheck-> bindParam(':installed_pack_version', $value[2], PDO::PARAM_STR);
+		$sqlcheck-> bindParam(':pack_sys_id', $test, PDO::PARAM_INT);
 		$sqlcheck->execute();
 		$result = $sqlcheck->fetchAll(PDO::FETCH_NUM);
 		if (in_array(0,$result[0]))
 		{
 			try {
-				$query = $dbh->prepare('INSERT INTO installed_package (installed_pack_category, installed_pack_name, installed_pack_version, installed_pack_summary) VALUES (:installed_pack_cat, :installed_pack_name, :installed_pack_version, NULL)');
+				$query = $dbh->prepare('INSERT INTO installed_package (installed_pack_category, installed_pack_name, installed_pack_version, installed_pack_summary, pack_sys_id) VALUES (:installed_pack_cat, :installed_pack_name, :installed_pack_version, NULL, :pack_sys_id)');
 				$query-> bindParam(':installed_pack_cat', $value[0], PDO::PARAM_STR);
 				$query-> bindParam(':installed_pack_name', $value[1], PDO::PARAM_STR);
 				$query-> bindParam(':installed_pack_version', $value[2], PDO::PARAM_STR);
+				$query-> bindParam(':pack_sys_id', $test, PDO::PARAM_INT);
 				//$query-> bindParam(':pack_expl', NULL, PDO::PARAM_STR);
 				$query->execute();
 			} catch(PDOException $ex) {
--- webd/dbupdate-pdo.php
+++ webd/dbupdate-pdo.php
@@ -21,24 +21,26 @@ array(PDO::ATTR_EMULATE_PREPARES => false,
 } catch (PDOException $e) {
  exit('データベース接続に失敗しました'.$e->getMessage());
 }
-
+$test=1;
 $data=$server->get_all_packages();
 foreach ($data as $value){
 	try {
-		$sqlcheck = $dbh->prepare("SELECT COUNT(*) FROM pack_info WHERE pack_category = :pack_cat AND pack_name = :pack_name AND pack_version = :pack_version;");
+		$sqlcheck = $dbh->prepare("SELECT COUNT(*) FROM pack_info WHERE pack_category = :pack_cat AND pack_name = :pack_name AND pack_version = :pack_version AND pack_sys_id = :pack_sys_id;");
 		//connect as appropriate as above
 		$sqlcheck-> bindParam(':pack_cat', $value[0], PDO::PARAM_STR);
 		$sqlcheck-> bindParam(':pack_name', $value[1], PDO::PARAM_STR);
 		$sqlcheck-> bindParam(':pack_version', $value[2], PDO::PARAM_STR);
+		$sqlcheck-> bindParam(':pack_sys_id', $test, PDO::PARAM_INT);
 		$sqlcheck->execute();
 		$result = $sqlcheck->fetchAll(PDO::FETCH_NUM);
 		if (in_array(0,$result[0]))		
 		{
 			try {
-				$query = $dbh->prepare('INSERT INTO pack_info (pack_category, pack_name, pack_version, pack_summary) VALUES (:pack_cat, :pack_name, :pack_version, NULL)');
+				$query = $dbh->prepare('INSERT INTO pack_info (pack_category, pack_name, pack_version, pack_summary, pack_sys_id) VALUES (:pack_cat, :pack_name, :pack_version, NULL, :pack_sys_id);');
 				$query-> bindParam(':pack_cat', $value[0], PDO::PARAM_STR);
 				$query-> bindParam(':pack_name', $value[1], PDO::PARAM_STR);
 				$query-> bindParam(':pack_version', $value[2], PDO::PARAM_STR);
+				$query-> bindParam(':pack_sys_id', $test, PDO::PARAM_INT);
 				//$query-> bindParam(':pack_expl', NULL, PDO::PARAM_STR);
 				echo var_dump($query);
 				$query->execute(); //invalid query!
