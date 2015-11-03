42b26bd6729a6c9981ea9a5464412f091f8eb764
alice ferrazzi <alice.ferrazzi@gmail.com>
2015-11-04 03:49:36 +0900

    added test installing script

---
+++ webd/test_install.php
@@ -0,0 +1,26 @@
+<?php
+require_once "includes/session.php";
+require_once "/includes/jsonRPCClient.php";
+require_once __DIR__ . '/includes/DBaction.php';
+$dba = new DBaction();
+$dbh = $dba->Connect();
+
+$me = new Session();
+$me->is_session_started();
+$user_id = 'test';
+
+$stm = $dbh->prepare("select * from machine_information WHERE user_id=:user_id;");
+$stm-> bindParam(':user_id', $user_id, PDO::PARAM_STR);
+$stm->execute();
+$data = $stm->fetchAll();
+$cnt  = count($data); //in case you need to count all rows
+foreach ($data as $i => $row)
+        $js_host=$row['ipaddress'];
+        $js_port=$row['port'];
+
+$server= new jsonRPCClient("http://$js_host:$js_port");
+ini_set('max_execution_time', 0); //300 seconds = 5 mi.12
+
+$data=$server->install_package('games-misc/fortune-mod');
+var_dump($data);
+?>
--- webd/test_session.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once "includes/session.php";
-$me = new Session();
-$me->is_session_started();
-$me->set_user_id('test');
-print $me->get_user_id();
-?>
