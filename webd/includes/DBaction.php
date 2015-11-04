5ef774af2092e2f8b87298d040f7543317ab3778
alice ferrazzi <alice.ferrazzi@gmail.com>
2015-11-05 00:08:42 +0900

    db refactor

--- webd/includes/DBaction.php
+++ webd/includes/DBaction.php
@@ -6,15 +6,52 @@ class DbAction {
     {
         require_once '/connect.php';
 
-        $dsn = "mysql:dbname=$db_name;host=$db_host;charset=utf8";
+        if (!empty($db_name)) {
+            if (!empty($db_host)) {
+                $dsn = "mysql:dbname=$db_name;host=$db_host;charset=utf8";
+            }
+        }
         //データベース接続
         try {
-                $dbh = new PDO($dsn, $db_user, $db_pass,
-                array(PDO::ATTR_EMULATE_PREPARES => false,
-                    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+            if (!empty($db_user)) {
+                if (!empty($db_pass)) {
+                    if (!empty($dsn)) {
+                        $dbh = new PDO($dsn, $db_user, $db_pass,
+                        array(PDO::ATTR_EMULATE_PREPARES => false,
+                            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+                    }
+                }
+            }
         } catch (PDOException $e) {
             exit('データベース接続に失敗しました' . $e->getMessage());
         }
         return $dbh;
+    }
+
+    public function CreateDbTable($table,$fields,$dbh)
+    {
+
+        $sql = "CREATE TABLE IF NOT EXISTS `$table` (";
+        $pk  = '';
+
+        foreach($fields as $field => $type)
+        {
+            $sql.= "`$field` $type,";
+
+            if (preg_match('/AUTO_INCREMENT/i', $type))
+            {
+                $pk = $field;
+            }
+        }
+
+        $sql = rtrim($sql,',') . ', PRIMARY KEY (`'.$pk.'`)';
+
+        $sql .= ") CHARACTER SET utf8 COLLATE utf8_general_ci";
+        try {
+            $dbh->query($sql); //invalid query!
+        } catch(PDOException $ex) {
+            echo "An Error occured!"; //user friendly message
+            some_logging_function($ex->getMessage());
+        }
     }
 }
