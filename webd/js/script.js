b46470f6dea0e56c6aafd83bdf636603e40c282d
depra95 <acc.depra95@gmail.com>
2015-11-06 14:40:14 +0900

    script.jsの構文修正 navigationに左右移動アニメーションを追加

--- webd/js/script.js
+++ webd/js/script.js
@@ -1,14 +1,40 @@
-$(document).ready(function () {
+$(function () {
+	//topbarのメニューボタン
 	$('.menu-button').click(function () {
 		var popup = $(this).children('.menu-popup');
-		if($(popup).css("display")=="none"){
+		if ($(popup).css("display") == "none") {
 			//menu-popupをすべて閉じる,popupの切替
-			$('.menu-popup').animate({height:"hide"}, {duration:0, easing:"easeOutCubic"});
+			$('.menu-popup').animate({height: "hide"}, {duration: 0, easing: "easeOutCubic"});
+			//モバイル幅の場合の処理
+			if (window.matchMedia('(max-width: 480px)').matches) {
+				//ナビゲーションが開かれていれば閉じる
+				if ($('.navigation').css("left") == "0px") {
+					$('.navigation').animate({left: "-=200px"}, {duration: 300, easing: "easeOutCubic"});
+					//$('.navigation').delay(300)[0].removeAttr('style');
+				}
+			}
 			//popupを開く
-			 $(popup).animate({height:"show"}, {duration:300, easing:"easeOutCubic"});
-		}else if($(popup).css("display")!="none"){
+			$(popup).css("display", "block");
+			//$(popup).animate({height:"show"}, {duration:300, easing:"easeOutCubic"});
+		}else if ($(popup).css("display") != "none") {
 			//popupが表示されてたら閉じる
-			$(popup).animate({height:"hide"}, {duration:300, easing:"easeOutCubic"});
-		};
+			//$(popup).animate({height:"hide"}, {duration:300, easing:"easeOutCubic"});
+			$(popup).css("display", "none");
+		}
     });
+	//左ナビゲーション
+	$('.navigation-toggle').click(function () {
+		//css leftの位置で開閉を判定
+		if ($('.navigation').css("left") != "0px") {
+			//	menu-popupをすべて閉じる モバイルで干渉するため
+			$('.menu-popup').css("display", "none");
+			$('.navigation').animate({left: "+=200px"}, {duration: 300, easing: "easeOutCubic"});
+		}else if ($('.navigation').css("left") == "0px") {
+			//一回閉じるとデスクトップ幅にした場合ナビゲーションが開けなくなるためStyleを削除
+			$('.navigation').animate({left: "-=200px"}, {duration: 300, easing: "easeOutCubic"}).delay(300)[0].removeAttr('style');
+			//.attr
+			//innerwidth
+			//ready load
+		}
+	});
 });
