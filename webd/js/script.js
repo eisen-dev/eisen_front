a903d2c02ac2eb4e47cd717e8a1ea362e2fa62dc
depra95 <acc.depra95@gmail.com>
2015-11-09 04:44:49 +0900

    ウィンドウ幅を変更しメディアクエリが切り替わった際の処理を追記

--- webd/js/script.js
+++ webd/js/script.js
@@ -5,20 +5,15 @@ $(function () {
 		if ($(popup).css("display") == "none") {
 			//menu-popupをすべて閉じる,popupの切替
 			$('.menu-popup').animate({height: "hide"}, {duration: 0, easing: "easeOutCubic"});
-			//モバイル幅の場合の処理
-			if (window.matchMedia('(max-width: 480px)').matches) {
-				//ナビゲーションが開かれていれば閉じる
-				if ($('.navigation').css("left") == "0px") {
-					$('.navigation').animate({left: "-=200px"}, {duration: 300, easing: "easeOutCubic"});
-					//$('.navigation').delay(300)[0].removeAttr('style');
-				}
+			//モバイル幅でnavigationが開いていた場合の処理
+			if (window.matchMedia('(max-width: 480px)').matches && $('.navigation').css("left") == "0px") {
+				//navigationを閉じる
+				$('.navigation').animate({left: "-200px"}, {duration: 300, easing: "easeOutCubic"});
 			}
 			//popupを開く
 			$(popup).css("display", "block");
-			//$(popup).animate({height:"show"}, {duration:300, easing:"easeOutCubic"});
 		}else if ($(popup).css("display") != "none") {
 			//popupが表示されてたら閉じる
-			//$(popup).animate({height:"hide"}, {duration:300, easing:"easeOutCubic"});
 			$(popup).css("display", "none");
 		}
     });
@@ -28,13 +23,24 @@ $(function () {
 		if ($('.navigation').css("left") != "0px") {
 			//	menu-popupをすべて閉じる モバイルで干渉するため
 			$('.menu-popup').css("display", "none");
-			$('.navigation').animate({left: "+=200px"}, {duration: 300, easing: "easeOutCubic"});
+			$('.navigation').animate({left: "0px"}, {duration: 300, easing: "easeOutCubic"});
 		}else if ($('.navigation').css("left") == "0px") {
-			//一回閉じるとデスクトップ幅にした場合ナビゲーションが開けなくなるためStyleを削除
-			$('.navigation').animate({left: "-=200px"}, {duration: 300, easing: "easeOutCubic"}).delay(300)[0].removeAttr('style');
-			//.attr
-			//innerwidth
-			//ready load
+			$('.navigation').animate({left: "-200px"}, {duration: 300, easing: "easeOutCubic"});
+		}
+	});
+
+	//ウィンドウ幅が切り替わった際のナビゲーション位置リセット
+	$(window).resize(function () {
+		var w = $(window).Width();
+		//innerwidthもある
+		var mobile = 480;
+		var desktop = 768;
+		var navw = 200;
+		if (w <= mobile) {
+			$('.navigation').css("left","-200px");
+		}
+		if (w >= mobile) {
+			$('.navigation').css("left","0px");
 		}
 	});
 });
