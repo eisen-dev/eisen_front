b7f78a6b5b639c18a2e674c130addc0c428b3b08
depra95 <acc.depra95@gmail.com>
2015-11-10 08:34:15 +0900

    navigation幅の指定を改善

--- webd/js/script.js
+++ webd/js/script.js
@@ -1,6 +1,7 @@
 $(function () {
 	//topbarのメニューボタン
 	$('.menu-button').click(function () {
+		var navw = $(".navigation").width();
 		var popup = $(this).children('.menu-popup');
 		if ($(popup).css("display") == "none") {
 			//menu-popupをすべて閉じる,popupの切替
@@ -8,7 +9,7 @@ $(function () {
 			//モバイル幅でnavigationが開いていた場合の処理
 			if (window.matchMedia('(max-width: 480px)').matches && $('.navigation').css("left") == "0px") {
 				//navigationを閉じる
-				$('.navigation').animate({left: "-200px"}, {duration: 300, easing: "easeOutCubic"});
+				$('.navigation').animate({left: "-" + navw + "px"}, {duration: 300, easing: "easeOutCubic"});
 			}
 			//popupを開く
 			$(popup).css("display", "block");
@@ -19,25 +20,26 @@ $(function () {
     });
 	//左ナビゲーション
 	$('.navigation-toggle').click(function () {
+		var navw = $(".navigation").width();
 		//css leftの位置で開閉を判定
 		if ($('.navigation').css("left") != "0px") {
 			//	menu-popupをすべて閉じる モバイルで干渉するため
 			$('.menu-popup').css("display", "none");
 			$('.navigation').animate({left: "0px"}, {duration: 300, easing: "easeOutCubic"});
 		}else if ($('.navigation').css("left") == "0px") {
-			$('.navigation').animate({left: "-200px"}, {duration: 300, easing: "easeOutCubic"});
+			$('.navigation').animate({left: "-" + navw + "px"}, {duration: 300, easing: "easeOutCubic"});
 		}
 	});
-
 	//ウィンドウ幅が切り替わった際のナビゲーション位置リセット
 	$(window).resize(function () {
+		var navw = $(".navigation").width();
 		var w = $(window).Width();
 		//innerwidthもある
 		var mobile = 480;
 		var desktop = 768;
 		var navw = 200;
 		if (w <= mobile) {
-			$('.navigation').css("left","-200px");
+			$('.navigation').css("left", "-" + navw + "px");
 		}
 		if (w >= mobile) {
 			$('.navigation').css("left","0px");
