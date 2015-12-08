f960abba17230e9867a15e433ce2b34b5a2386ca
alice ferrazzi <alice.ferrazzi@gmail.com>
2015-12-08 15:44:13 +0900

    changing with typescript as back compatible with javascript
    For using typescript codestyle and feature but still be
    able to use javascript code style.

--- webd/parts/scripts.php
+++ webd/parts/scripts.php
@@ -1,3 +1,3 @@
     <script type="text/javascript" src="includes/jquery/jquery-2.1.4.min.js"></script>
     <script type="text/javascript" src="includes/jquery/jquery-ui.min.js"></script>
-	<script type="text/javascript" src="js/script.js"></script>
+	<script type="text/javascript" src="ts/script.js"></script>
