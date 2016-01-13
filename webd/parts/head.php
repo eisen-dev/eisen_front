b0651826be1ac649a3f6efa40c58c92d8de0d010
depra95 <acc.depra95@gmail.com>
2016-01-13 10:16:12 +0900

    Added favicon

--- webd/parts/head.php
+++ webd/parts/head.php
@@ -9,6 +9,7 @@
 	$me->is_session_started();
     ?>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+    <link rel="icon" href="images/favicon.png" type="image/png">
 	<link rel="stylesheet" type="text/css" href="includes/normalize.css">
 	<link rel="stylesheet" href="includes/font-awesome-4.3.0/css/font-awesome.min.css">
 	<link rel="stylesheet" type="text/css" href="sass/style.css">
