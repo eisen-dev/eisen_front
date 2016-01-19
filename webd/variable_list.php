7339fef907b3d33aba0ccc2c3c33429f756ee29c
depra95 <acc.depra95@gmail.com>
2016-01-19 16:12:48 +0900

    Fixed had tag problem

--- webd/variable_list.php
+++ webd/variable_list.php
@@ -1,14 +1,12 @@
 <!DOCTYPE html>
 <html lang="ja">
 <head>
-    <meta charset="UTF-8">
-    <title>マシン管理</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
-    <link rel="stylesheet" type="text/css" href="includes/normalize.css">
-    <link rel="stylesheet" type="text/css"
-          href="includes/font-awesome-4.3.0/css/font-awesome.min.css">
-    <link rel="stylesheet" type="text/css" href="sass/style.css">
-    <link rel="stylesheet" type="text/css" href="includes/jquery-ui.css"/>
+<?php
+// タイトル
+$title = "テンプレート";
+require_once __DIR__ .'/parts/head.php';
+?>
+
     <style>
         #popup {
             display: none;
