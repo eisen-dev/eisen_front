6bb428f1cc15b4bb07eb53482c26e021c0bcbbfe
alice ferrazzi <alice.ferrazzi@gmail.com>
2016-01-21 11:34:05 +0900

    Fix phpunit install error update php to php 5.6

--- vagrant/script.sh
+++ vagrant/script.sh
@@ -16,7 +16,7 @@ echo -e "\n--- Install base packages ---\n"
 apt-get -y install vim curl build-essential python-software-properties git > /dev/null 2>&1
 
 echo -e "\n--- Add some repos to update our distro ---\n"
-add-apt-repository ppa:ondrej/php5 > /dev/null 2>&1
+add-apt-repository ppa:ondrej/php5-5.6 > /dev/null 2>&1
 add-apt-repository ppa:chris-lea/node.js > /dev/null 2>&1
 
 echo -e "\n--- Updating packages list ---\n"
