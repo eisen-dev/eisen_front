93a23e0451020f67e5c67851620c790db98ac170
alice ferrazzi <alice.ferrazzi@gmail.com>
2016-01-25 01:51:15 +0900

    package action now need to call the rest package resource

--- webd/includes/package_action/package_action.php
+++ webd/includes/package_action/package_action.php
@@ -16,63 +16,31 @@ $rest = new restclient();
 $me->start_session();
 $me->is_session_started();
 
-$target = array();
+$target = [];
 if(isset($_GET['package'])){
     $package = htmlspecialchars($_GET["package"]);
+    echo ($package);
 }
 if(isset($_GET['action'])){
     $action = htmlspecialchars($_GET["action"]);
+    echo ($action);
 }
 if(isset($_GET['target'])){
-    $target["ipaddress"] = htmlspecialchars($_GET["target"]);
+    $target_host = htmlspecialchars($_GET["target"]);
+    echo ( $target["ipaddress"]);
 }
 if(isset($_GET['module'])){
     $target["module"] = htmlspecialchars($_GET["module"]);
 }
 
-
-
-var_dump($package);
-var_dump($action);
-var_dump($target);
-
-// get the machine manager variables
-// is defined in host_manager.php
-
-$module=$_SESSION["manager"]["module"];
-$ipaddress=$_SESSION["manager"]["ipaddress"];
-$port=$_SESSION["manager"]["port"];
-$username=$_SESSION["manager"]["username"];
-$password=$_SESSION["manager"]["password"];
-$machine_host=$_SESSION["manager"];
-var_dump($machine_host);
-
-// Get the target host variable
-// is defined in target_list.php
-
-$target_module="portage";
-$target_host=$target["ipaddress"];
-$os = $target['os'];
-
-#TODO we need to filter the variables for security issue
-# like injections
-if ($action=='install') {
-    $command = "package==" . $package . "";
-}
-elseif($action=='update') {
-    $command = "package==" . $package . "";
-}
-elseif ($action == 'delete') {
-    $command = "package==" . $package . " state=absent depclean=yes";
-}
-#install can be more than 30 sec
-ini_set('max_execution_time', 300); //300 seconds = 5 minutes
-$task_id=$rest->task_register($ipaddress,$port,$username,$password,$target_host,$command,$target_module);
-$package=$rest->tasks_run($ipaddress,$port,$username,$password,$task_id);
-$rest->tasks_delete($ipaddress,$port,$username,$password,$task_id);
-
-#better var_dump for httpful
-foreach ($package as $i=>$row) {
-    var_dump($i);
-    var_dump($row);
-}
+$task_id = $rest->package_Action(
+$rest_host,
+$rest_port,
+$username,
+$password,
+$target_host,
+$targetOs,
+$packageName,
+$packageVersion,
+$packageAction
+);
