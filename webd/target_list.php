28b2fce4aac46a713ff7a9b937302622a7776a83
alice ferrazzi <alice.ferrazzi@gmail.com>
2016-01-27 15:42:15 +0900

    Cleaned and fixed code style removed close form tag that was not needed.

--- webd/target_list.php
+++ webd/target_list.php
@@ -2,10 +2,10 @@
 <!DOCTYPE html>
 <html lang="ja">
 <head>
-<title><?php echo _('Target list'); ?></title>
-<?php
-require_once __DIR__ .'/parts/head.php';
-?>
+    <title><?php echo _('Target list'); ?></title>
+    <?php
+        require_once __DIR__ .'/parts/head.php';
+    ?>
     <style>
         #popup {
             display: none;
@@ -45,55 +45,51 @@ $dbh = $dba->Connect();
     <?php require_once __DIR__ . '/parts/navigation.php'; ?>
     <div class="contentswrapper menu-set">
         <main class="contents">
-                <div class="content-header">
-                    <!-- page title -->
-                    <h2 class="title content-header-title"><?php echo _('target host list'); ?></h2>
-                    <!-- page general setting button and useful buttons area -->
-                    <div class="content-header-buttons">
-                        <div class="content-header-button">
-                            <!-- header button area, example for add new machine button. -->
-                            <button class="btn btn-sm"><i class="fa fa-plus"></i>新規マシン追加</button>
-                        </div>
-                         <!-- setting button, open setting modal. this is optional button. -->
-                        <button class="content-header-setting" data-modal="open"
-                     data-modal-target="target_host_list-setting"><i class="fa fa-cog"></i></button>
+            <div class="content-header">
+                <!-- page title -->
+                <h2 class="title content-header-title"><?php echo _('target host list'); ?></h2>
+                <!-- page general setting button and useful buttons area -->
+                <div class="content-header-buttons">
+                    <div class="content-header-button">
+                        <!-- header button area, example for add new machine button. -->
+                        <button class="btn btn-sm"><i class="fa fa-plus"></i>新規マシン追加</button>
                     </div>
+                    <!-- setting button, open setting modal. this is optional button. -->
+                    <button class="content-header-setting" data-modal="open"
+                            data-modal-target="target_host_list-setting"><i class="fa fa-cog"></i></button>
                 </div>
+            </div>
+            <form action="includes/target_host_checkbox.php" method="post">
 
-
-                <form action="includes/target_host_checkbox.php" method="post">
-
-                    <div class="n-list-tools">
+                <div class="n-list-tools">
                     <!-- new list control tools -->
-					<form id="example-form">
-						<div class="n-list-toolbar">
-							<div class="n-list-action">
+                    <form id="example-form">
+                        <div class="n-list-toolbar">
+                            <div class="n-list-action">
                                 <!-- dropdown list and submit button.-->
-                          <select name="list-action" class="input-list">
-                                <option value="0"><?php echo _('select action'); ?></option>
-                                <option value="1"><?php echo _('package list'); ?></option>
-                                <option value="2"><?php echo _('task list'); ?></option>
-                                <option value="3"><?php echo _('settings'); ?></option>
-                            </select>
-								<button type="submit" value="適用" class="btn btn-sm">実行</button>
+                                <select name="list-action" class="input-list">
+                                    <option value="0"><?php echo _('select action'); ?></option>
+                                    <option value="1"><?php echo _('package list'); ?></option>
+                                    <option value="2"><?php echo _('task list'); ?></option>
+                                    <option value="3"><?php echo _('settings'); ?></option>
+                                </select>
+                                <button type="submit" value="適用" class="btn btn-sm">実行</button>
                                 <!-- additional control button is here,use button tag -->
                                 <button class="btn btn-sm"><i class="fa fa-refresh"></i>リストを更新</button>
-							</div>
-							<div class="n-searchbox">
-								<input type="text" class="n-search-box-input" placeholder="全てのパッケージを検索">
+                            </div>
+                            <div class="n-searchbox">
+                                <input type="text" class="n-search-box-input" placeholder="全てのパッケージを検索">
                                 <!-- search button -->
-								<button type="submit" name="submit" class="n-search-button"><i class="fa fa-search"></i></button>
+                                <button type="submit" name="submit" class="n-search-button">
+                                    <i class="fa fa-search"></i></button>
                                 <!-- optional filter button -->
-								<button type="button" class="n-filter-button"><i class="fa fa-filter"></i></button>
-							</div>
-						</div>
+                                <button type="button" class="n-filter-button"><i class="fa fa-filter"></i></button>
+                            </div>
+                        </div>
                         <!-- optional filter area -->
                         <!-- optional filter area end -->
-                    </form>
-					<!--  new list control tools end-->
+                        <!--  new list control tools end-->
                 </div>
-
-
                 <div class="table-wrapper">
                     <table class="table">
                         <thead>
@@ -105,11 +101,11 @@ $dbh = $dba->Connect();
                                 </div>
                             </th>
                             <th><?php echo _('ip address'); ?></th>
-                            <th>port</th>
-                            <th>groups</th>
-                            <th>os</th>
-                            <th>machine id</th>
-                            <th>status id<th>
+                            <th><?php echo _('port'); ?></th>
+                            <th><?php echo _('groups'); ?></th>
+                            <th><?php echo _('os'); ?></th>
+                            <th><?php echo _('machine id'); ?></th>
+                            <th><?php echo _('status id'); ?><th>
                         </tr>
                         </thead>
                         <tbody>
@@ -118,33 +114,33 @@ $dbh = $dba->Connect();
                         $hosts = new TargetHostController();
                         $data = $hosts->get_TargetHost($user_id);
                         foreach ($data as $i => $manager) {
-                            foreach ($manager as $x => $row) {
+                            foreach ($manager as $x => $targetHost) {
                                 $table = '<tr class="cell-which-triggers-popup">
-                            <td class="list-data-ctrl">
-                            <div class="list-data-cbox">
-                                <input type="checkbox" id="cbox-' . $row['host_id'] .
-                                    '" value="' . $row['host_id'] . '" name="check[]">
-                                <label for="cbox-' . $row['host_id'] . '">
-                            <div class="select"></div></label></div>';
+                        <td class="list-data-ctrl">
+                        <div class="list-data-cbox">
+                            <input type="checkbox" id="cbox-' . $targetHost['host_id'] .
+                                    '" value="' . $targetHost['host_id'] . '" name="check[]">
+                            <label for="cbox-' . $targetHost['host_id'] . '">
+                        <div class="select"></div></label></div>';
                                 $table .= '</td>';
                                 $table .= '<td class="ipaddress"><input type="hidden" id="test"' .
-                                   ' value="' . $row['ipaddress'] . '" name="managerHostAddress[]">' .
-                                    $row['ipaddress'] . '</td>';
-                                $table .= '<td class="port">' . $row['port'] . '</td>';
-                                $table .= '<td class="groups">' . $row['groups'] . '</td>';
-                                $table .= '<td class="os">' . $row['os'] . '</td>';
+                                    ' value="' . $targetHost['ipaddress'] . '" name="targetHostIp[]">' .
+                                    $targetHost['ipaddress'] . '</td>';
+                                $table .= '<td class="port">' . $targetHost['port'] . '</td>';
+                                $table .= '<td class="groups">' . $targetHost['groups'] . '</td>';
+                                $table .= '<td class="os">' . $targetHost['os'] . '</td>';
                                 $table .= '<td class="ipaddress"><input type="hidden" id="test"' .
-                                    ' value="' . $row['machine_id'] . '" name="managerHostId[]">' .
-                                    $row['machine_id'] . '</td>';
-                                $table .= '<td class="status_id">' . $row['status_id'] . '</td>';
+                                    ' value="' . $targetHost['machine_id'] . '" name="managerHostId[]">' .
+                                    $targetHost['machine_id'] . '</td>';
+                                $table .= '<td class="status_id">' . $targetHost['status_id'] . '</td>';
                                 echo($table);
                             }
                         }
                         ?>
                         </tbody>
                     </table>
-                    </div>
-                </form>
+                </div>
+            </form>
         </main>
     </div>
 </div>
